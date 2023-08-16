0e044e8cd173cd62103c608d4a50af3611a4c20c
VictorUdeme <snazzydo9@gmail.com>
2023-08-16 16:49:03 +0100

    Task 06

--- SQL_introduction/6-list_values.sql
+++ SQL_introduction/6-list_values.sql
@@ -1,3 +1,33 @@
 --This script that lists all rows of the table first_table from the database hbtn_0c_0
+#!/usr/bin/env python3
+
 import mysql.connector
-SELECT * FROM hbtn_0c_0.first_table;
+
+def main():
+    # Get the database name from the command line arguments
+    database_name = input("Enter the database name: ")
+
+    # Connect to the MySQL server
+    connection = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="password",
+        database=database_name
+    )
+
+    # Get a cursor object
+    cursor = connection.cursor()
+
+    # Execute the SQL query to list all the rows of the first_table table
+    cursor.execute("SELECT * FROM first_table")
+
+    # Fetch all the rows from the result set
+    rows = cursor.fetchall()
+
+    # Print all the rows
+    for row in rows:
+        print(row)
+
+if __name__ == "__main__":
+    main()
+
