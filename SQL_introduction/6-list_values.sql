07d41367dd99919e825af0ca88fa52e22843f4c0
VictorUdeme <snazzydo9@gmail.com>
2023-08-16 16:51:10 +0100

    Task 06

--- SQL_introduction/6-list_values.sql
+++ SQL_introduction/6-list_values.sql
@@ -1,6 +1,8 @@
 --This script that lists all rows of the table first_table from the database hbtn_0c_0
 #!/usr/bin/env python3
 
+#!/usr/bin/env python3
+
 import mysql.connector
 
 def main():
@@ -19,7 +21,7 @@ def main():
     cursor = connection.cursor()
 
     # Execute the SQL query to list all the rows of the first_table table
-    cursor.execute("SELECT * FROM first_table")
+    cursor.execute("SELECT * FROM FIRST_TABLE")
 
     # Fetch all the rows from the result set
     rows = cursor.fetchall()
@@ -29,5 +31,5 @@ def main():
         print(row)
 
 if __name__ == "__main__":
-    main()
+    main() 
 
