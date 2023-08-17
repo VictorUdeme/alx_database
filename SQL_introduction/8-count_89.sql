38c93a661f010d9c2cc0950e3cd029a42da62d38
Snazzydo <snazzydo9@gmail.com>
2023-08-17 03:46:25 +0100

    Task 08

---
+++ SQL_introduction/8-count_89.sql
@@ -0,0 +1,7 @@
+/* a script that displays the number of records with 
+id = 89 in the table first_table of the database hbtn_0c_0 
+in your MySQL server.*/
+
+SELECT COUNT(*) AS count_of_records
+FROM hbtn_0c_0.first_table
+WHERE id = 89;
