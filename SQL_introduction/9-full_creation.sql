b6acd71ef65d1931a3219d7db78cee0133dad091
Snazzydo <snazzydo9@gmail.com>
2023-08-17 13:25:47 +0100

    Task 9

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,12 +1,12 @@
 -- Create table second_table if it doesn't exist
-CREATE TABLE IF NOT EXISTS hbtn_0c_0.second_table (
+CREATE TABLE IF NOT EXISTS second_table (
     id INT,
     name VARCHAR(256),
     score INT
 );
 
 -- Insert rows into second_table
-INSERT INTO hbtn_0c_0.second_table (id, name, score) VALUES
+INSERT INTO second_table (id, name, score) VALUES
     (1, 'John', 10),
     (2, 'Alex', 3),
     (3, 'Bob', 14),
