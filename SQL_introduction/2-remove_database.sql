1c33319cf9f89b2ca4176661c0c1128f28d25fbd
Snazzydo <snazzydo9@gmail.com>
2023-08-17 12:47:58 +0100

    Task 02

--- SQL_introduction/2-remove_database.sql
+++ SQL_introduction/2-remove_database.sql
@@ -1,3 +1,3 @@
---This Script delete the hbtn_0c_0 database
-#!/bin/bash
-mysql -uroot -p -e "DROP DATABASE IF EXISTS hbtn_0c_0;"
+-- Attempt to drop the database hbtn_0c_0
+-- We will use the DROP DATABASE statement, which will not raise an error if the database doesn't exist
+DROP DATABASE IF EXISTS hbtn_0c_0;
