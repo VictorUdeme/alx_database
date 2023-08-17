dbf445cb0e1f901a9834fadd769ccc6d0debddfa
Snazzydo <snazzydo9@gmail.com>
2023-08-17 02:54:53 +0100

    Task 01

--- SQL_introduction/1-create_database_if_missing.sql
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -1,7 +1,2 @@
 --This script creates the database hbtn_0c_0 in MySQL server.
-
-
-
-CREATE DATABASE IF NOT EXISTS hbtn_0c_0
-CHARACTER SET utf8
-COLLATE utf8_general_ci;
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
