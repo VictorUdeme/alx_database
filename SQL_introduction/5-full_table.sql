a87050915f05335c64ca82650cf62e0f827db07a
Snazzydo <snazzydo9@gmail.com>
2023-08-17 13:01:45 +0100

    Task 5

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,9 +1,2 @@
 -- Define the database and table names
-SET @db_name := 'hbtn_0c_0';
-SET @table_name := 'first_table';
-
--- Print the full description of the table
-SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_DEFAULT, COLUMN_KEY, EXTRA
-FROM information_schema.COLUMNS
-WHERE TABLE_SCHEMA = @db_name AND TABLE_NAME = @table_name;
-
+SHOW CREATE TABLE first_table;
