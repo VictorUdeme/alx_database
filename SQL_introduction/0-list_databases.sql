b05ce4faf1f64bff1cefc3d4a0b48cc7b039716b
Snazzydo <snazzydo9@gmail.com>
2023-08-17 12:43:04 +0100

    Task 0

--- SQL_introduction/0-list_databases.sql
+++ SQL_introduction/0-list_databases.sql
@@ -1,7 +1,2 @@
---@This script lists all databases of the MySQL server.
-
-import mysql.connector
---The `list_databases()` function takes no arguments and returns a list of all databases in the MySQL server.
-
-#!/bin/bash
-mysql -uroot -p -e "SHOW DATABASES;"
+-- List all databases
+SHOW DATABASES;
