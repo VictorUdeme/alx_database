3bec2460d9d96ccbc03fe3af3c01dcf1602135d3
Snazzydo <snazzydo9@gmail.com>
2023-08-17 02:51:45 +0100

    Task 0

--- SQL_introduction/0-list_databases.sql
+++ SQL_introduction/0-list_databases.sql
@@ -1,4 +1,4 @@
---This script lists all databases of the MySQL server.
+--@This script lists all databases of the MySQL server.
 
 import mysql.connector
 --The `list_databases()` function takes no arguments and returns a list of all databases in the MySQL server.
