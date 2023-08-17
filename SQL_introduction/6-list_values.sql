ab95153d4ccc849f2e8e53094e6ea0cde340d035
Snazzydo <snazzydo9@gmail.com>
2023-08-17 13:10:00 +0100

    Task 6

--- SQL_introduction/6-list_values.sql
+++ SQL_introduction/6-list_values.sql
@@ -1,6 +1,8 @@
--- Use the specified database
+-- Recreate the database hbtn_0c_0
+CREATE DATABASE hbtn_0c_0;
+
+-- Use the recreated database
 USE hbtn_0c_0;
 
 -- List all rows of the first_table
 SELECT * FROM first_table;
-
